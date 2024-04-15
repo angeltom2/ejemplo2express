35b36bf0065c3ef65c3e00378f5f6faed41105b7
Rodriguez <anasfajardol@gmail.com>
2024-04-15 16:17:44 -0500

    login y register

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -10,3 +11,41 @@ router.post("/animals", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 module.exports = router;
+
+router.get("/animals",verifyToken, (req, res) => {
+    animalSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
